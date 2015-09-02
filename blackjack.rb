de9ebd59695e63d801140bcbcc14e41f38eb328f
joshneipp <joshneipp@gmail.com>
2015-09-02 15:39:57 -0400

    Convert aces to 11.

--- blackjack.rb
+++ blackjack.rb
@@ -1,24 +1,34 @@
 #define a sample hash
-
 #soft = {dealer => {player => "action"}}
-puts "Play some blackjack! Enter your two cards, and the dealer's card."
-puts "For face cards, please enter 10"
-puts "For ace, please enter A"
-puts "First card: "
 
 blackjack = Hash.new
 blackjack = {"soft" => {4 => {19 => "Stand"}}}
 
-first = gets.chomp.to_i
-puts "#{first}"
+def evaluate(card)
+  if card == "A"
+    card = 11
+  else
+    card
+  end
+end
 
-puts "Second card: "
-second = gets.chomp.to_i
-puts "#{second}"
-player_hand = first + second
-puts "Your hand is #{player_hand}"
+puts "Play some blackjack! Enter your two cards, and the dealer's card."
+puts "For face cards, please enter 10"
+puts "For ace, please enter A"
 
-puts "Dealer card: "
-dealer = gets.chomp.to_i
+puts "What is your first card?"
+first = gets.chomp
+first_card = evaluate(first).to_i
 
-puts "You should #{blackjack["soft"][dealer][player_hand]}"
+puts "What is your second card?"
+second = gets.chomp
+second_card = evaluate(second).to_i
+
+puts "What is the dealer's card?"
+dealer = gets.chomp
+dealer_card = evaluate(dealer).to_i
+
+player_hand = first_card + second_card
+
+puts "Your hand is #{player_hand} and dealer has #{dealer_card}"
+puts "You should #{blackjack["soft"][dealer_card][player_hand]}"
